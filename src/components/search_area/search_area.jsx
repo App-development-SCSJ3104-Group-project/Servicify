fae985eb1b74989492bed031d1242eca01accc28
Jalalajlan <aajalal@graduate.utm.my>
2021-05-12 00:22:38 +0800

    landing page tabs only updated

--- src/components/search_area/search_area.jsx
+++ src/components/search_area/search_area.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./search_area.css";
-import NavigationTab from "./../navbar/tab";
+import NavigationTab from "./tab";
 
 class SearchArea extends Component {
   constructor(props) {
@@ -24,32 +24,6 @@ class SearchArea extends Component {
             currentTab={true}
             lastTab={false}
             tabName={"overview"}
-          />
-          <NavigationTab
-            currentTab={false}
-            lastTab={false}
-            tabName={"services"}
-          />
-          <NavigationTab currentTab={false} lastTab={false} tabName={"posts"} />
-          <NavigationTab
-            currentTab={false}
-            lastTab={false}
-            tabName={"orders"}
-          />
-          <NavigationTab
-            currentTab={false}
-            lastTab={false}
-            tabName={"favorite"}
-          />
-          <NavigationTab
-            currentTab={false}
-            lastTab={false}
-            tabName={"requests"}
-          />
-          <NavigationTab
-            currentTab={false}
-            lastTab={true}
-            tabName={"profile"}
           />
         </div>
       </div>
