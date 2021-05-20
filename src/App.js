259b25a14b5a7e09e24e92d0ae0d2b277ada58db
Jalalajlan <aajalal@graduate.utm.my>
2021-05-21 00:01:50 +0800

    fake commit

--- src/App.js
+++ src/App.js
@@ -7,11 +7,7 @@ import CustomerOrderScreen from "../src/pages/customer_order/CustomerOrdersScree
 import { Route } from "react-router-dom";
 import CustomerMain from "./pages/customer_main_screen/customer_main_screen";
 import CustomerServiceMainPage from "./pages/customer_service_main_page/customer_service_main_page";
-<<<<<<< HEAD
-import Test from "./pages/test/test";
-=======
 import CustomerServiceProviderPage from "./pages/customer_service_provider_page/customer_service_provider_page";
->>>>>>> edd3ee0534ea51efd1c5aceccff891fc3de3bec0
 function App() {
   return (
     <div className="App">
@@ -23,7 +19,6 @@ function App() {
         path="/customer_orders_screen"
         component={CustomerOrderScreen}
       ></Route>
-      <Route exact path="/test" component={Test}></Route>
     </div>
   );
 }
