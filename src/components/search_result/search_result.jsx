d0d9c70b74853c68530630bb25a3884a0789ea7b
Jalalajlan <aajalal@graduate.utm.my>
2021-06-07 05:34:40 +0800

    refactor landing page search result with dynamic data as well as inner components and svgs

--- src/components/search_result/search_result.jsx
+++ src/components/search_result/search_result.jsx
@@ -1,43 +1,103 @@
 import React from "react";
 import "./search_result.css";
-import FilterForm from "./form";
-import FilterCategory from "./filtercategory";
-import Card from "./card";
+import Avatar from "./avatar";
+import StarIcon from "./inner_components/star";
+import Tag from "./tag";
+import users from "./dummydata";
 
 const SearchResult = () => {
+  const filterSkills = [
+    "Electrical",
+    "Mechnical",
+    "House Cleaning",
+    "Pet care",
+  ];
+
   return (
-    <div className="row search-section">
-      <div className="col-md-3 filter-section">
-        <p className="filter">Filters: </p>
-        <hr />
-        <p className="filter">Price: </p>
-        <div className="arranged-forms">
-          <FilterForm placeholder="Min" />
-          <p className="m-2">To</p>
-          <FilterForm placeholder="Max" />
+    <div className="search__result_container_a">
+      <div className="search__result_filter-section">
+        <div className="filter-section_title">
+          <p>Filters:</p>
         </div>
-        <ul className="category-list">
-          <FilterCategory categoryName="Plumbing" />
-          <FilterCategory categoryName="House cleaning" />
-          <FilterCategory categoryName="Pet care" />
-          <FilterCategory categoryName="Farming" />
-          <FilterCategory categoryName="Planting" />
-        </ul>
+        <div className="filter-section_content-1">
+          <p>Hourly Rate:</p>
+        </div>
+        <div className="filter-section_content-2">
+          <input
+            className="filter-section_content-2__input"
+            placeholder="Min"
+            type="text"
+          />
+          <input
+            className="filter-section_content-2__input"
+            placeholder="Max"
+            type="text"
+          />
+        </div>
+        <div className="filter-section_content-3">
+          <p className="filter-section_content-3_skills">Skills:</p>
+          {filterSkills.map((skill) => (
+            <FilterSkills key={skill} skill={skill} />
+          ))}
+        </div>
       </div>
-      <div className="col-md-8 result-section">
-        <div className="search-result-top">
-          <div className="result-title">
-            <p>Top Results:</p>
-          </div>
-          <div className="page-number">
-            <p>1-20 page</p>
+      <div className="search__result_results-section">
+        <div className="search__result_results-section-title">
+          <p>Top Results:</p>
+        </div>
+        {users.map((user) => (
+          <UserResultCard key={user.id} {...user} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const UserResultCard = (user) => {
+  return (
+    <div className="search__result_card_container">
+      <div className="search__result_card_header">
+        <div className="search__result_card_header-left-section">
+          <Avatar
+            source={
+              "https://pixinvent.com/materialize-material-design-admin-template/app-assets/images/user/12.jpg"
+            }
+            width={"80px"}
+            height={"80px"}
+          />
+          <div className="search__result_card_header-sub-section">
+            <span>
+              {user.userLocation}, {user.userCity} @{user.username}
+            </span>
+            <div className="search__result_card_header-sub-section-stars">
+              {user.userRate.map((star) => (
+                <StarIcon key={star} color={star && "#E5A800"} />
+              ))}
+            </div>
           </div>
         </div>
-        <hr />
-        <Card />
-        <Card />
-        <Card />
+        <div className="search__result_card_header-right-section">
+          ${user.userFee}USD
+        </div>
       </div>
+      <div className="search__result_card_content">
+        <p>{user.userBio}</p>
+      </div>
+      <div className="search__result_card_footer">
+        {user.userCommonTags.map((tag) => (
+          <Tag key={tag} color="#EEC37E" tag={tag}></Tag>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const FilterSkills = (props) => {
+  return (
+    <div className="skill_box-filter">
+      <input type="checkbox" id="checkbox_filter-section" />
+      <span className="skill_name">{props.skill}</span>
+      <br />
     </div>
   );
 };
