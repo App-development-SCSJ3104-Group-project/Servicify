0dd217209ff0a698d0b80703abafb6dc4f393163
MarwanMostafa Taha <tmarawan11@gmail.com>
2021-06-13 14:35:00 +0300

    modified overview icons and added some animations

--- src/components/overview/overview.jsx
+++ src/components/overview/overview.jsx
@@ -1,13 +1,36 @@
 import React from "react";
 import "./overview.css";
-import EmailIcon from "./inner_components/email";
-import PlusIcon from "./inner_components/plus";
-import PencilIcon from "./inner_components/pencil";
-import SendIcon from "./inner_components/send";
+import EmailIcon from "../../icons/email.svg";
+import PlusIcon from "../../icons/add.svg";
+import PencilIcon from "../../icons/writing.svg";
+import SendIcon from "../../icons/send.svg";
 
 const Overview = () => {
   const card_Description =
     "write a post for the service to enable freelance to see your requested services, write a post for the service to enable freelance to see your requested services";
+  
+  let overViewCards = [
+    {card_Description :
+      "write a post for the service to enable freelance to see your requested services, write a post for the service to enable freelance to see your requested services",
+      logo:<PencilIcon></PencilIcon>
+    },
+     {card_Description :
+       "write a post for the service to enable freelance to see your requested services, write a post for the service to enable freelance to see your requested services",
+      logo:<EmailIcon></EmailIcon>
+       
+    },
+      {card_Description :
+        "write a post for the service to enable freelance to see your requested services, write a post for the service to enable freelance to see your requested services",
+      logo:<PlusIcon></PlusIcon>
+        
+    },
+       {card_Description :
+         "write a post for the service to enable freelance to see your requested services, write a post for the service to enable freelance to see your requested services",
+      logo:<SendIcon></SendIcon>
+         
+    },
+       
+  ]
 
   return (
     <div className="Overview__section_container_card">
@@ -17,21 +40,24 @@ const Overview = () => {
         </p>
       </div>
       <div className="Overview__section_cards_information_container">
-        <OverviewCard desc={card_Description} logo={<PencilIcon />} />
-        <OverviewCard desc={card_Description} logo={<EmailIcon />} />
-        <OverviewCard desc={card_Description} logo={<PlusIcon />} />
-        <OverviewCard desc={card_Description} logo={<SendIcon />} />
+
+        {
+          overViewCards.map((card,index)=><OverviewCard desc={card.card_Description} logo={card.logo} index={index} />)
+        }
+      
       </div>
     </div>
   );
 };
 
 const OverviewCard = (props) => {
+
+  console.log(props)
   return (
-    <div className="Overview__section_card_information">
-      <div className="Overview__section_card_icon-section">{props.logo}</div>
-      <div className="Overview__section_card_desc-section">
-        <p className="Overview__section_card_desc-section_box">{props.desc}</p>
+    <div className=" Overview__section_card_information" >
+      <div className="Overview__section_card_icon-section" style={{animationDelay:`${props.index*.35}s`}}> <img src={props.logo.type}></img> </div>
+      <div className="Overview__section_card_desc-section"style={{animationDelay:`${props.index*.5}s`}} >
+        <p className=" Overview__section_card_desc-section_box" >{props.desc}</p>
       </div>
     </div>
   );
