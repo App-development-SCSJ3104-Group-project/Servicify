f2bfaccf1ec91c47cbeea5e97e74bef972f64d71
ajlanjalal <aajalal@graduate.utm.my>
2021-06-19 00:30:47 +0800

    re-installed node_modules
    I re-installed the all node_modules  but still doesn't work. I can access my pages that has not been added to the react route other not accessible

--- src/pages/customer_main_screen/customer_main_screen.jsx
+++ src/pages/customer_main_screen/customer_main_screen.jsx
@@ -2,16 +2,13 @@ import React, { Component } from "react";
 import Overview from "../../components/overview/overview";
 import Categories from "../../components/categories/categories";
 import SearchResult from "../../components/search_result/search_result";
-import Template from "../../components/template/template"
+import Template from "../../components/template/template";
 
 import "./landing_page.scss";
 import { connect } from "react-redux";
 
-
 class CustomerMain extends Component {
-
   render() {
-
     return (
       <Template>
         <div className="landing-page">
@@ -21,13 +18,12 @@ class CustomerMain extends Component {
           <hr />
           <SearchResult />
         </div>
-
       </Template>
     );
   }
 }
 
-export default CustomerMain
+export default CustomerMain;
 // const mapStateToProps = (state) => {
 //   return {
 //     name: state.main.posts
