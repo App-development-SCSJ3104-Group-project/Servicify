b042e97945a26047de7344d09fa09f39eb3a198c
ajlanjalal <aajalal@graduate.utm.my>
2021-06-27 03:48:06 +0800

    proposal form control and submission

--- src/pages/service_provider_make_proposal_screen/sp_make_proposal_form.jsx
+++ src/pages/service_provider_make_proposal_screen/sp_make_proposal_form.jsx
@@ -2,9 +2,9 @@ import React, { useState } from "react";
 import Nav from "./../../components/navbar/navbar";
 import "./service_provider_make_proposal.css";
 import PostCard from "./../../components/post_card/post_card";
-// import { useForm, useFieldArray } from "react-hook-form";
 import { v4 as uuidv4 } from "uuid";
-
+import { Link } from "react-router-dom";
+//
 const ProposalForm = (props) => {
   const postsList = JSON.parse(localStorage.getItem("posts"));
   const post = postsList.find((post) => post._id === props.match.params._id);
@@ -36,11 +36,31 @@ const ManageProposalForm = (props) => {
   const [inputFields, setInputFields] = useState([
     { id: uuidv4(), stepDesc: "" },
   ]);
+  const [diagnosisFee, setDiagnosisFee] = useState("");
+  const [paymentMethod, setPaymentMethod] = useState();
+  const [description, setDescription] = useState();
+  const [serviceProviderId, setServiceProviderId] = useState("A18CS4042");
+
+  const proposalInfo = {};
 
   // submission
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(inputFields);
+
+    if (!parseInt(diagnosisFee)) {
+      alert("Enter cost of diagnosis");
+    } else if (paymentMethod === "" || paymentMethod == "payment") {
+      alert("Please select payement method");
+    } else if (description === "") {
+      alert("please Enter Description");
+    } else {
+      proposalInfo["serviceProviderId"] = serviceProviderId;
+      proposalInfo["diagnosisFee"] = diagnosisFee;
+      proposalInfo["paymentMethod"] = paymentMethod;
+      proposalInfo["description"] = description;
+      proposalInfo["steps"] = inputFields;
+      console.log(proposalInfo);
+    }
   };
 
   const handleAddFields = () => {
@@ -75,24 +95,28 @@ const ManageProposalForm = (props) => {
       <div className="proposal__form_body_section">
         <input
           type="text"
+          name="serviceProviderId"
+          value={serviceProviderId}
+          hidden
+        />
+        <input
+          type="text"
           className="proposal__form_body_section-input_field-1"
           placeholder="Cost of diagnosis"
           name="fee"
           autoComplete="off"
+          onChange={(e) => setDiagnosisFee(e.target.value)}
         />
         <br />
         <div className="select">
           <select
             className="proposal__form_body_section-input_field-copied"
             name="payment"
+            onChange={(e) => setPaymentMethod(e.target.value)}
           >
-            <option value="payment" selected disabled>
-              Payment method
-            </option>
+            <option value="payment">Payment method</option>
             <option value="Cash">Cash</option>
-            <option value="onlineBanking">Online banking</option>
-            <option value="FBX">FBX service</option>
-            <option value="creditCard">credit card</option>
+            <option value="card">Credit/debit card</option>
           </select>
         </div>
         <textarea
@@ -100,6 +124,7 @@ const ManageProposalForm = (props) => {
           placeholder="Describe your proposal here"
           name="description"
           wrap="off"
+          onChange={(e) => setDescription(e.target.value)}
         ></textarea>
 
         {/* dynmic  */}
@@ -111,7 +136,7 @@ const ManageProposalForm = (props) => {
               value={inputField.stepDesc}
               onChange={(event) => handleChangeInput(inputField.id, event)}
               className="proposal-form-stepDesc"
-              placeholder={`Enter Step ${index + 1} Description`}
+              placeholder={`*Optional Enter Step ${index + 1} Description`}
               autoComplete="off"
             />
             {index + 1 === 1 ? (
@@ -130,11 +155,13 @@ const ManageProposalForm = (props) => {
       </div>
       <ProposalFormPostArea post={props.post} />
       <div className="proposal__footArea_submit_section">
-        <input
-          className="proposal__footArea_submit_section-button"
-          type="submit"
-          value="Back"
-        />
+        <Link to="/customer_post_screen">
+          <input
+            className="proposal__footArea_submit_section-button"
+            type="submit"
+            value="Back"
+          />
+        </Link>
         <input
           className="proposal__footArea_submit_section-button"
           type="submit"
