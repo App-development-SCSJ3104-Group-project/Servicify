ed1ecfacdd118c2d660954267813bbf223e187a1
Ahmad-Elraqab <65804256+Ahmad-Elraqab@users.noreply.github.com>
2021-07-02 04:37:54 +0300

    frontend css props changed

--- src/pages/customer_manage_requests/manage_proposals/manage_proposals.jsx
+++ src/pages/customer_manage_requests/manage_proposals/manage_proposals.jsx
@@ -21,60 +21,65 @@ class ManageProposals extends Component {
     }
 
     render() {
-        const { requests, cancelRequest } = this.props
+        const { requests, cancelRequest, loading } = this.props
         return (
             <DashboardCard label={"Orders history"} >
 
                 <div className="list_of_cards">
-
                     {
-                        requests.map(prop => (
-                            [
-                                <Card imgHeight="15.0rem" scaleUp={true} imgWidth="15.0rem" width="50%" image_src={profileIcon} imgHsize="8rem" imgWsize="8rem" data={prop.name} callback={this.onClickCard}>
+                        loading ?
+                            <div className="loading-div">
+                                <div className="loader">Loading...</div>
+                            </div>
+                            :
+                            typeof requests === "string" ? <h3>{requests}</h3> :
+                                requests.map(prop => (
+                                    [
+                                        <Card imgHeight="15.0rem" scaleUp={true} imgWidth="15.0rem" width="50%" image_src={profileIcon} imgHsize="8rem" imgWsize="8rem" data={prop.name} callback={this.onClickCard}>
 
-                                    <div className="order_header_card">
-                                        <div className="order_header_card_left">
-                                            <div className="part_one">
-                                                <h3>{prop.serviceProvider.firstName + " " + prop.serviceProvider.lastName}</h3>
-                                                <div className="rate_div">
-                                                    <h3>4.5</h3>
-                                                    <img src={star} alt="" />
+                                            <div className="order_header_card">
+                                                <div className="order_header_card_left">
+                                                    <div className="part_one">
+                                                        <h3>{prop.serviceProvider.firstName + " " + prop.serviceProvider.lastName}</h3>
+                                                        <div className="rate_div">
+                                                            <h3>4.5</h3>
+                                                            <img src={star} alt="" />
+                                                        </div>
+                                                    </div>
+                                                    <h3>{prop.time} / {prop.location} / {prop.payment}</h3>
+                                                </div>
+                                                <div className="right_float_button">
+                                                    {prop.status === "Pending" ?
+                                                        <div onClick={() => {
+                                                            const data = JSON.parse(localStorage.getItem("user"))
+                                                            cancelRequest(prop._id, data._id); setTimeout(() => {
+                                                                window.location.reload()
+                                                            }, 500);
+                                                        }}>
+                                                            <IconButton innerText={"Cancel"} heightDiv="4.0rem" widthDiv="15.0rem"
+                                                                borderRadius="5rem" backgroundColor="#1E2833" src={null} />
+                                                        </div>
+                                                        : null
+                                                    }
+                                                    <div className="proposal_status" style={{ cursor: `pointer`, width: "15rem" }}>
+                                                        <h4>{prop.status}</h4>
+                                                    </div>
                                                 </div>
                                             </div>
-                                            <h3>{prop.time} / {prop.location} / {prop.payment}</h3>
-                                        </div>
-                                        <div className="right_float_button">
-                                            {prop.status === "Pending" ?
-                                                <div onClick={() => {
-                                                    const data = JSON.parse(localStorage.getItem("user"))
-                                                    cancelRequest(prop._id, data._id); setTimeout(() => {
-                                                        window.location.reload()
-                                                    }, 500);
-                                                }}>
-                                                    <IconButton innerText={"Cancel"} heightDiv="4.0rem" widthDiv="15.0rem"
-                                                        borderRadius="5rem" backgroundColor="#1E2833" src={null} />
-                                                </div>
-                                                : null
-                                            }
-                                            <div className="proposal_status" style={{ cursor: `pointer`, width: "15rem" }}>
-                                                <h4>{prop.status}</h4>
+                                            <div className="toggle_card">
+                                                <br />
+                                                <h3>City: {prop.location} </h3>
+                                                <h3>Diagnosing fees: {prop.fees}</h3>
+                                                <h3>Payment: {prop.payment}</h3>
+                                                <h3>Description:{prop.description}</h3>
+                                                <h3>Provision date:{prop.provisionDate}</h3>
+                                                <h3>Time:{prop.time}</h3>
+                                                <h3>Feedback: Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse mollitia officiis ipsum. Praesentium, consequuntur. Mollitia aspernatur quibusdam cupiditate laboriosam quis?</h3>
+                                                <h3>Rate: 3</h3>
                                             </div>
-                                        </div>
-                                    </div>
-                                    <div className="toggle_card">
+                                        </Card>,
                                         <br />
-                                        <h3>City: {prop.location} </h3>
-                                        <h3>Diagnosing fees: {prop.fees}</h3>
-                                        <h3>Payment: {prop.payment}</h3>
-                                        <h3>Description:{prop.description}</h3>
-                                        <h3>Provision date:{prop.provisionDate}</h3>
-                                        <h3>Time:{prop.time}</h3>
-                                        <h3>Feedback: Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse mollitia officiis ipsum. Praesentium, consequuntur. Mollitia aspernatur quibusdam cupiditate laboriosam quis?</h3>
-                                        <h3>Rate: 3</h3>
-                                    </div>
-                                </Card>,
-                                <br />
-                            ]))
+                                    ]))
                     }
                 </div>
 
--- src/pages/dashboard_manage_requests/manage_request/manage_request.jsx
+++ src/pages/dashboard_manage_requests/manage_request/manage_request.jsx
@@ -33,60 +33,61 @@ class ManageRequests extends Component {
                 <div className="loader">Loading...</div>
               </div>
               :
-              requests.map(prop => (
-                [
-                  <Card imgHeight="15.0rem" scaleUp={true} imgWidth="15.0rem" width="50%" image_src={profileIcon} imgHsize="8rem" imgWsize="8rem" data={prop.name} callback={this.onClickCard}>
+              typeof requests === "string" ? <h3>{requests}</h3> :
+                requests.map(prop => (
+                  [
+                    <Card imgHeight="15.0rem" scaleUp={true} imgWidth="15.0rem" width="50%" image_src={profileIcon} imgHsize="8rem" imgWsize="8rem" data={prop.name} callback={this.onClickCard}>
 
-                    <div className="order_header_card">
-                      <div className="order_header_card_left">
-                        <div className="part_one">
-                          <h3>{prop.serviceProvider.firstName + " " + prop.serviceProvider.lastName}</h3>
-                          <div className="rate_div">
-                            <h3>4.5</h3>
-                            <img src={star} alt="" />
-                          </div>
-                        </div>
-                        <h3>{prop.time} / {prop.location} / {prop.payment}</h3>
-                      </div>
-                      <div className="right_float_button">
-
-                        {
-                          prop.status === "Pending" ?
-                            [<div onClick={() => {
-                              acceptRequest(prop._id, JSON.parse(localStorage.getItem("user"))._id, "Accepted"); setTimeout(() => {
-                                window.location.reload()
-                              }, 500);
-                            }}>
-                              <IconButton innerText={"Accept"} heightDiv="4.0rem" widthDiv="15.0rem"
-                                borderRadius="5rem" backgroundColor="#1E2833" src={null} />
-                            </div>,
-                            <div onClick={() => {
-                              acceptRequest(prop._id, JSON.parse(localStorage.getItem("user"))._id, "Rejected"); setTimeout(() => {
-                                window.location.reload()
-                              }, 500);
-                            }}>
-                              <IconButton innerText={"Reject"} heightDiv="4.0rem" widthDiv="15.0rem"
-                                borderRadius="5rem" backgroundColor="#1E2833" src={null} />
-                            </div>] : <div className="proposal_status" style={{ cursor: `pointer`, width: "15rem" }}>
-                              <h4>{prop.status}</h4>
+                      <div className="order_header_card">
+                        <div className="order_header_card_left">
+                          <div className="part_one">
+                            <h3>{prop.serviceProvider.firstName + " " + prop.serviceProvider.lastName}</h3>
+                            <div className="rate_div">
+                              <h3>4.5</h3>
+                              <img src={star} alt="" />
                             </div>
-                        }
+                          </div>
+                          <h3>{prop.time} / {prop.location} / {prop.payment}</h3>
+                        </div>
+                        <div className="right_float_button">
+
+                          {
+                            prop.status === "Pending" ?
+                              [<div onClick={() => {
+                                acceptRequest(prop._id, JSON.parse(localStorage.getItem("user"))._id, "Accepted"); setTimeout(() => {
+                                  window.location.reload()
+                                }, 500);
+                              }}>
+                                <IconButton innerText={"Accept"} heightDiv="4.0rem" widthDiv="15.0rem"
+                                  borderRadius="5rem" backgroundColor="#1E2833" src={null} />
+                              </div>,
+                              <div onClick={() => {
+                                acceptRequest(prop._id, JSON.parse(localStorage.getItem("user"))._id, "Rejected"); setTimeout(() => {
+                                  window.location.reload()
+                                }, 500);
+                              }}>
+                                <IconButton innerText={"Reject"} heightDiv="4.0rem" widthDiv="15.0rem"
+                                  borderRadius="5rem" backgroundColor="#1E2833" src={null} />
+                              </div>] : <div className="proposal_status" style={{ cursor: `pointer`, width: "15rem" }}>
+                                <h4>{prop.status}</h4>
+                              </div>
+                          }
+                        </div>
                       </div>
-                    </div>
-                    <div className="toggle_card">
-                      <br />
-                      <h3>City: {prop.location} </h3>
-                      <h3>Diagnosing fees: {prop.fees}</h3>
-                      <h3>Payment: {prop.payment}</h3>
-                      <h3>Description:{prop.description}</h3>
-                      <h3>Provision date:{prop.provisionDate}</h3>
-                      <h3>Time:{prop.time}</h3>
-                      <h3>Feedback: Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse mollitia officiis ipsum. Praesentium, consequuntur. Mollitia aspernatur quibusdam cupiditate laboriosam quis?</h3>
-                      <h3>Rate: 3</h3>
-                    </div>
-                  </Card>,
-                  <br />
-                ]))
+                      <div className="toggle_card">
+                        <br />
+                        <h3>City: {prop.location} </h3>
+                        <h3>Diagnosing fees: {prop.fees}</h3>
+                        <h3>Payment: {prop.payment}</h3>
+                        <h3>Description:{prop.description}</h3>
+                        <h3>Provision date:{prop.provisionDate}</h3>
+                        <h3>Time:{prop.time}</h3>
+                        <h3>Feedback: Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse mollitia officiis ipsum. Praesentium, consequuntur. Mollitia aspernatur quibusdam cupiditate laboriosam quis?</h3>
+                        <h3>Rate: 3</h3>
+                      </div>
+                    </Card>,
+                    <br />
+                  ]))
           }
         </div>
       </DashboardCard>
