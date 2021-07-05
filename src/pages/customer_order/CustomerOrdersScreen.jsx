fb1810b2f32cc69b13ebbabfd7357b104a12f99c
ajlanjalal <aajalal@graduate.utm.my>
2021-07-05 10:29:35 +0800

    change in proposal accpting

--- src/pages/customer_order/CustomerOrdersScreen.jsx
+++ src/pages/customer_order/CustomerOrdersScreen.jsx
@@ -3,16 +3,15 @@ import "./CustomerOrdersScreen.scss";
 import CustomButton from "../../components/button/button";
 import Order from "../../components/order/order";
 
-import Diagnosing from "../../icons/prescription.svg"
-import PopUp from "./InnerComponents/PopUp/PopUp"
-import { loadData } from "../../redux/orders/orders_action"
-import Template from "../../components/template/template"
-import { giveFeedback } from "../../redux/orders/orders_action"
+import Diagnosing from "../../icons/prescription.svg";
+import PopUp from "./InnerComponents/PopUp/PopUp";
+import { loadData } from "../../redux/orders/orders_action";
+import Template from "../../components/template/template";
+import { giveFeedback } from "../../redux/orders/orders_action";
 import { connect } from "react-redux";
 
-
 class CustomerOrdersScreen extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super(props);
     this.state = {
       activeTab: "OrdersList",
@@ -40,7 +39,7 @@ class CustomerOrdersScreen extends React.Component {
           isActive: true,
         },
       ],
-      orderId: null
+      orderId: null,
     };
   }
 
@@ -59,54 +58,67 @@ class CustomerOrdersScreen extends React.Component {
   };
 
   handlePopUpInput = (childData) => {
-    console.log(this.state.orderId)
-    console.log(childData.text)
-    console.log(childData.rating)
+    console.log(this.state.orderId);
+    console.log(childData.text);
+    console.log(childData.rating);
 
-    this.props.giveFeedback(this.state.orderId, childData.text, childData.rating)
-  }
+    this.props.giveFeedback(
+      this.state.orderId,
+      childData.text,
+      childData.rating
+    );
+  };
   closePopUp = (popUpType, orderId) => {
     this.setState({
       popUpToggle: false,
-      popUpType: popUpType
-    })
-  }
+      popUpType: popUpType,
+    });
+  };
   openPopUp = (popUpType, orderId) => {
     this.setState({
       popUpToggle: true,
       popUpType: popUpType,
-      orderId
-    })
-
-  }
+      orderId,
+    });
+  };
 
   checkTab = () => {
-
     //returning the first order within the ongoing orders
-    const { ordersList } = this.props
-    const currentOrder = typeof ordersList !== "string" ? ordersList.filter((order) => order.ongoing === true)[0] : null
+    const { ordersList } = this.props;
+    const currentOrder =
+      typeof ordersList !== "string"
+        ? ordersList.filter((order) => order.ongoing === true)[0]
+        : null;
 
     //deciding on tabs content, if orders list , orderslist will get populated
     if (this.state.activeTab != "OrdersList") {
+      if (
+        currentOrder === null ||
+        currentOrder === undefined ||
+        currentOrder === []
+      ) {
+        return (
+          <div className="orders-list-inner-section__content-container__status__inner  animate__animated animate__zoomInDown">
+            <img
+              src={Diagnosing}
+              alt=""
+              className="orders-list-inner-section__content-container__status__img "
+            />
 
-      if (currentOrder === null || currentOrder === undefined || currentOrder === []) {
-
-        return <div className="orders-list-inner-section__content-container__status__inner  animate__animated animate__zoomInDown">
-
-          <img src={Diagnosing} alt="" className="orders-list-inner-section__content-container__status__img " />
-
-          <div className="orders-list-inner-section__content-container__status__inner__diagnosis-info ">
-            <div>No ongoing Orders</div>
-            <div></div>
+            <div className="orders-list-inner-section__content-container__status__inner__diagnosis-info ">
+              <div>No ongoing Orders</div>
+              <div></div>
+            </div>
           </div>
-        </div>
-
+        );
       } else {
         return (
-
           <div className="orders-list-inner-section__content-container__status__inner  animate__animated animate__zoomInDown">
-
-            <img src={Diagnosing} alt="" className="orders-list-inner-section__content-container__status__img " />
+            <img
+              src={Diagnosing}
+              alt=""
+              className="orders-list-inner-section__content-container__status__img "
+            />
 
             <div className="orders-list-inner-section__content-container__status__inner__diagnosis-info ">
               <div>Diagnosing the problem</div>
@@ -114,34 +126,34 @@ class CustomerOrdersScreen extends React.Component {
             </div>
             <Order {...currentOrder} orderType="order-status"></Order>
           </div>
-        )
-
+        );
       }
-
     }
-  }
+  };
   componentWillMount() {
     const { loadData } = this.props;
-    const data = JSON.parse(localStorage.getItem("user"))
+    const data = JSON.parse(localStorage.getItem("user"));
 
-    loadData(data._id)
-
+    loadData(data._id);
   }
   render() {
-
-
     const { innerTabs } = this.state;
     const { handleTabChanges } = this;
 
-
-
     return (
       <Template route="orders">
-
         <div className="customer-order">
           <div className="order-list-section">
-
-            {this.state.popUpToggle === true ? <PopUp popUpType={this.state.popUpType} closeCallBack={this.closePopUp} submitCallBack={this.handlePopUpInput} Header={this.state.popUpType === "feedback" ? "Description" : "Reason"}></PopUp> : null}
+            {this.state.popUpToggle === true ? (
+              <PopUp
+                popUpType={this.state.popUpType}
+                closeCallBack={this.closePopUp}
+                submitCallBack={this.handlePopUpInput}
+                Header={
+                  this.state.popUpType === "feedback" ? "Description" : "Reason"
+                }
+              ></PopUp>
+            ) : null}
 
             <div className="orders-list-inner-section">
               <div className="orders-list-inner-section__content-container">
@@ -164,54 +176,60 @@ class CustomerOrdersScreen extends React.Component {
                   })}
                 </div>
                 <div className="orders-list-inner-section__content-container__orders ">
-                  {
-
-                    this.state.activeTab === "OrdersList" ?
-
-                      <div className="orders-list-inner-section__content-container__orders__animation-container animate__animated animate__zoomIn">
-                        {
-                          typeof this.props.ordersList !== "string" ? this.props.ordersList.map((order, index) => {
-                            return <Order {...order} order={order} id={index} className="" giveFeedBackOnClick={this.openPopUp} cancelOnClick={this.openPopUp}></Order>
-                          }) : <div className="orders-list-inner-section__content-container__orders__no-orders">{this.props.ordersList}</div>
-                        }
-                      </div>
-                      : null
-                  }
+                  {this.state.activeTab === "OrdersList" ? (
+                    <div className="orders-list-inner-section__content-container__orders__animation-container animate__animated animate__zoomIn">
+                      {typeof this.props.ordersList !== "string" ? (
+                        this.props.ordersList.map((order, index) => {
+                          return (
+                            <Order
+                              {...order}
+                              order={order}
+                              id={index}
+                              className=""
+                              giveFeedBackOnClick={this.openPopUp}
+                              cancelOnClick={this.openPopUp}
+                            ></Order>
+                          );
+                        })
+                      ) : (
+                        <div className="orders-list-inner-section__content-container__orders__no-orders">
+                          {this.props.ordersList}
+                        </div>
+                      )}
+                    </div>
+                  ) : null}
                 </div>
                 <div className="orders-list-inner-section__content-container__status ">
-                  {
-
-                    this.checkTab()
-                  }
-
-
+                  {this.checkTab()}
                 </div>
               </div>
             </div>
           </div>
-
         </div>
       </Template>
-
     );
   }
 }
 
-
 const mapStateToProps = ({ ordersReducer, usersReducer }) => {
   return {
     ordersList: ordersReducer.ordersList,
-    userInAuth: usersReducer.userInAuth
-  }
-}
+    userInAuth: usersReducer.userInAuth,
+  };
+};
 
 const mapDispatchToProps = (dispatch) => {
-
   return {
     // import action from //???? action file
-    loadData: (userInAuth) => { dispatch(loadData(userInAuth)) },
-    giveFeedback: (id, description, rate) => { dispatch(giveFeedback(id, description, rate)) }
-
-  }
-}
-export default connect(mapStateToProps, mapDispatchToProps)(CustomerOrdersScreen)
+    loadData: (userInAuth) => {
+      dispatch(loadData(userInAuth));
+    },
+    giveFeedback: (id, description, rate) => {
+      dispatch(giveFeedback(id, description, rate));
+    },
+  };
+};
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CustomerOrdersScreen);
--- src/redux/requests/requests_action.js
+++ src/redux/requests/requests_action.js
@@ -1,81 +1,90 @@
-import axios from "axios"
-import { RequestsActionType } from "./request_action_type"
+import axios from "axios";
+import { RequestsActionType } from "./request_action_type";
 export const makeRequest = (data) => {
-    return (dispatch) => {
+  return (dispatch) => {
+    dispatch(setLoading(true));
+    const req = {
+      customerId: data.customerId,
+      serviceProviderId: data.serviceProviderId,
+      description: data.description,
+      fees: data.fees,
+      payment: data.payment,
+      location: data.location,
+    };
 
-        dispatch(setLoading(true))
-        const req = {
-            customerId: data.customerId,
-            serviceProviderId: data.serviceProviderId,
-            description: data.description,
-            fees: data.fees,
-            payment: data.payment,
-            location: data.location
-        }
-        
-        axios.post("https://service-backend-web.herokuapp.com/requests/create", req).then(res => {
-            dispatch(setLoading(false))
-
-        })
-
-    }
-}
+    axios
+      .post("https://service-backend-web.herokuapp.com/requests/create", req)
+      .then((res) => {
+        dispatch(setLoading(false));
+      });
+  };
+};
 export const setLoading = (value) => ({
-    type: RequestsActionType.SET_LOADING,
-    value
-})
+  type: RequestsActionType.SET_LOADING,
+  value,
+});
 
 export const setRequest = (value) => ({
-    type: RequestsActionType.GET_REQUESTS,
-    value
-})
+  type: RequestsActionType.GET_REQUESTS,
+  value,
+});
 
 export const getRequest = (id) => {
-    return (dispatch) => {
-        dispatch(setLoading(true))
+  return (dispatch) => {
+    dispatch(setLoading(true));
 
-        axios.get("https://service-backend-web.herokuapp.com/requests/?id=" + id + "&type=false").then(res => {
-
-            dispatch(setLoading(false))
-            dispatch(setRequest(res.data))
-        })
-
-    }
-}
+    axios
+      .get(
+        "https://service-backend-web.herokuapp.com/requests/?id=" +
+          id +
+          "&type=false"
+      )
+      .then((res) => {
+        dispatch(setLoading(false));
+        dispatch(setRequest(res.data));
+      });
+  };
+};
 export const getServiceProviderRequest = (id) => {
-    return (dispatch) => {
-        dispatch(setLoading(true))
+  return (dispatch) => {
+    dispatch(setLoading(true));
 
-        axios.get("https://service-backend-web.herokuapp.com/requests/?id=" + id + "&type=true").then(res => {
-          
-            dispatch(setLoading(false))
-            dispatch(setRequest(res.data))
-        })
-
-    }
-}
+    axios
+      .get(
+        "https://service-backend-web.herokuapp.com/requests/?id=" +
+          id +
+          "&type=true"
+      )
+      .then((res) => {
+        dispatch(setLoading(false));
+        dispatch(setRequest(res.data));
+      });
+  };
+};
 export const cancelRequest = (id, customerId) => {
-    return (dispatch) => {
-        dispatch(setLoading(true))
+  return (dispatch) => {
+    dispatch(setLoading(true));
 
-        axios.patch("https://service-backend-web.herokuapp.com/requests/" + id, { status: "Canceled" }).then(res => {
-
-            getRequest(customerId)
-            dispatch(setLoading(false))
-        })
-
-    }
-}
+    axios
+      .patch("https://service-backend-web.herokuapp.com/requests/" + id, {
+        status: "Canceled",
+      })
+      .then((res) => {
+        getRequest(customerId);
+        dispatch(setLoading(false));
+      });
+  };
+};
 export const acceptRequest = (id, customerId, status) => {
-    return (dispatch) => {
-        dispatch(setLoading(true))
+  return (dispatch) => {
+    dispatch(setLoading(true));
 
-        // axios.patch("https://service-backend-web.herokuapp.com/requests/" + id, { status: status }).then(res => {
-        axios.patch("http://localhost:5000/requests/" + id, { status: status }).then(res => {
-
-            getServiceProviderRequest(customerId)
-            dispatch(setLoading(false))
-        })
-
-    }
-}
+    // axios.patch("https://service-backend-web.herokuapp.com/requests/" + id, { status: status }).then(res => {
+    axios
+      .patch("http://localhost:5000/requests/" + id, { status: status })
+      .then((res) => {
+        getServiceProviderRequest(customerId);
+        dispatch(setLoading(false));
+      });
+  };
+};
