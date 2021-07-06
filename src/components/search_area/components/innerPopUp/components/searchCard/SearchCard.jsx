1038a0c9e6d80a8beba5c331fc03fbc822549f84
MarwanMostafa Taha <tmarawan11@gmail.com>
2021-07-06 04:25:13 +0200

    last push for real

--- src/components/search_area/components/innerPopUp/components/searchCard/SearchCard.jsx
+++ src/components/search_area/components/innerPopUp/components/searchCard/SearchCard.jsx
@@ -5,7 +5,7 @@ import { ReactComponent as Heart } from "../../../../../../icons/icons8-heart-30
 import Save from "../../../../../../icons/outline_book_white_24dp 1 (2).svg";
 import BlackStar from "../../../../../../icons/outline_star_black_24dp 1 (1).svg"
 import { Link } from "react-router-dom"
-
+import defaultImg from "../../../../../../icons/defaultUser.svg"
 
 const SearchCard = (props) => {
 
@@ -16,7 +16,7 @@ const SearchCard = (props) => {
         <Link className="search-card" to={{ pathname: "/customer_service_provider_page", state: { data: data } }} style={{ textDecoration: "none", color: "black" }}>
             <div className="search-card">
 
-                <img src={imgs} alt="img" className="search-card__img" />
+                <img style={{ borderRadius: "50%" }} src={data.imgSrc ? data.imgSrc : defaultImg} alt="img" className="search-card__img" />
                 <div className="search-card__text">
                     <div className="search-card__text__name">{data.firstName} {data.lastName}</div>
                     <div className="search-card__text__rate">{data.rate != null ? data.rate : "No Ratings"} <span> {data.rate != null ? <img src={BlackStar} alt="" className="search-card__text__rate__black-star" /> : null} </span></div>
